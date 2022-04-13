4a58e514de9a810140e56f764734afc56188247e
Rizwan Saeed <rizwan.saeed@finbourne.com>
2022-04-13 08:44:45 +0100

    CTECH-1082 fix retry header when str

--- fbnsdkutilities/utilities/retry.py
+++ fbnsdkutilities/utilities/retry.py
@@ -26,6 +26,13 @@ def retry(sdk):
 
                     # try after delay
                     elif retry_after is not None:
+
+                        if not isinstance(retry_after, float):
+                            try:
+                                retry_after = float(retry_after)
+                            except ValueError:
+                                raise ValueError(f"invalid Retry-After header value: {retry_after}")
+
                         sleep(retry_after)
 
                     # no retry header
@@ -33,4 +40,5 @@ def retry(sdk):
                         raise
 
         return __retry
+
     return _retry
